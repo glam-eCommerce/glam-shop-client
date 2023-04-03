904fd484047cb4b3ec2f024aba32ce9d6854e878
Faisal Samudra <isalsamudra@hotmail.com>
2023-04-04 03:22:20 +0800

    Updated client codes (removed redis to server)

--- src/components/shop/home/Promo.js
+++ src/components/shop/home/Promo.js
@@ -1,35 +1,6 @@
-import React, { useState, useEffect } from "react";
-import redis from "redis";
 
-const Promo = () => {
-  const [promo, setPromo] = useState("");
-
-  // connect to AWS redis
-  useEffect(() => {
-
-    const redisClient = redis.createClient({
-      socket: {
-        host: process.env.REDIS_HOST,
-        port: process.env.REDIS_PORT,
-      },
-    });
-
-    (async () => {
-      await redisClient.connect();
-    })();
-
-    redisClient.on("error", function (err) {
-      console.log(
-        "==========Could not establish a connection with redis.======="
-      );
-      console.log(err);
-    });
-
-    redisClient.get("promo", (err, reply) => {
-      setPromo(reply);
-    });
-  }, []);
-
+const Promo = ({promo}) => {
+  
   return <div class="promo">{promo}</div>;
 };
 
--- src/components/shop/home/index.js
+++ src/components/shop/home/index.js
@@ -1,17 +1,35 @@
-import React, { Fragment, createContext, useReducer } from "react";
+import React, { Fragment, createContext, useReducer, useEffect } from "react";
 import Layout from "../layout";
 import Slider from "./Slider";
 import ProductCategory from "./ProductCategory";
 import { homeState, homeReducer } from "./HomeContext";
 import SingleProduct from "./SingleProduct";
 import Promo from "./Promo";
+const apiURL = process.env.REACT_APP_API_URL;
 
 export const HomeContext = createContext();
 
 const HomeComponent = () => {
+  
+  const [promo, setPromo] = useState("");
+  
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(`${apiURL}/api/promo`);
+      setPromo(response.text);
+    } catch (error) {
+      console.log('Error fetching promo', error);
+    }
+  }
+  
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <Fragment>
-      <Promo/>
+      {/* Promo */}
+      <Promo promo={promo}/>
       <Slider />
       {/* Category, Search & Filter Section */}
       <section className="m-4 md:mx-8 md:my-6">
